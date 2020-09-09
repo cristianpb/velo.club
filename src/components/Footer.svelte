c18c9146c878079fc960b5c784c3f588cde3d896
cristianpb <felipebrokate@gmail.com>
2020-09-09 23:27:06 +0200

    🎉 Initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<style lang="postcss">
+  a {
+    @apply font-semibold;
+  }
+  a:hover {
+    @apply underline;
+  }
+</style>
+
+<footer class="bg-gray-200">
+  <p class="container text-lg py-10 max-w-4xl">
+    Want more?
+  </p>
+</footer>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Nav from '../components/Nav.svelte';
+  import Footer from '../components/Footer.svelte';
+
+  export let segment;
+</script>
+
+<div>
+  <Nav {segment} />
+
+  <main>
+    <slot />
+  </main>
+
+  <Footer />
+</div>
