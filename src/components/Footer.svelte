ea1e51974c8604663436a369f4be78012dc11918
cristianpb <felipebrokate@gmail.com>
2020-09-12 08:47:29 +0200

    💄 Change footer colors

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="bg-gray-200">
-  <p class="container max-w-4xl p-10 text-lg">
+  <p class="container max-w-4xl p-10 text-lg text-gray-600">
     Copyright © 2020 Velo Club SAS
   </p>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,30 @@
-<style lang="postcss">
-</style>
-
-<script>
-  import { slide } from 'svelte/transition';
-
-  let show = false;
-</script>
-
 <svelte:head>
   <title>Velo Club</title>
 </svelte:head>
 
 <div class="py-20" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)"
 >
-  <div class="container mx-auto px-6">
-    <h2 class="text-4xl font-bold mb-2 text-white">
+  <div class="container px-6 mx-auto">
+    <h2 class="mb-2 text-4xl font-bold text-white">
       La location de vélos à assistance électrique
     </h2>
-    <h3 class="text-2xl mb-8 text-gray-200">
+    <h3 class="mb-8 text-2xl text-gray-200">
       des 50€ par mois
     </h3>
-    <button class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+    <a href='offres' class="px-8 py-4 font-bold tracking-wider uppercase bg-white rounded-full shadow-lg">
       Rejoindre le club
-    </button>
+    </a>
   </div>
 </div>
 
-<section class="container mx-auto px-6 p-10">
-  <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">
+<section class="container p-10 px-6 mx-auto">
+  <h2 class="mb-8 text-4xl font-bold text-center text-gray-800">
    Avantages! 
   </h2>
-  <div class="flex items-center flex-wrap mb-20">
+  <div class="flex flex-wrap items-center mb-20">
     <div class="w-full md:w-1/2">
-      <h4 class="text-3xl text-gray-800 font-bold mb-3">En cas de crevasse, nous remplaçons votre vélo en 48h</h4>
-      <p class="text-gray-600 mb-8">Adieu la paperasse, les assurances en plus, les SAV interminables...
+      <h4 class="mb-3 text-3xl font-bold text-gray-800">En cas de crevasse, nous remplaçons votre vélo en 48h</h4>
+      <p class="mb-8 text-gray-600">Adieu la paperasse, les assurances en plus, les SAV interminables...
         Nos équipes vous répondent en moins de 20 minutes.</p>
     </div>
     <div class="w-full md:w-1/2">
@@ -41,20 +32,20 @@
     </div>
   </div>
 
-  <div class="flex items-center flex-wrap mb-20">
+  <div class="flex flex-wrap items-center mb-20">
     <div class="w-full md:w-1/2">
       <img src="{process.env.NODE_ENV === 'development' ? '' : '/velo.club'}/bike.svg" alt="Reporting" />
     </div>
-    <div class="w-full md:w-1/2 pl-10">
-      <h4 class="text-3xl text-gray-800 font-bold mb-3">Sans engagement</h4>
-      <p class="text-gray-600 mb-8">Vous pouvez changer ou rendre votre vélo à tout moment, sans frais. </p>
+    <div class="w-full pl-10 md:w-1/2">
+      <h4 class="mb-3 text-3xl font-bold text-gray-800">Sans engagement</h4>
+      <p class="mb-8 text-gray-600">Vous pouvez changer ou rendre votre vélo à tout moment, sans frais. </p>
     </div>
   </div>
 
-  <div class="flex items-center flex-wrap mb-20">
+  <div class="flex flex-wrap items-center mb-20">
     <div class="w-full md:w-1/2">
-      <h4 class="text-3xl text-gray-800 font-bold mb-3">Remplacement en cas de vol</h4>
-      <p class="text-gray-600 mb-8">En cas de vol, nous remplaçons votre appareil.</p>
+      <h4 class="mb-3 text-3xl font-bold text-gray-800">Remplacement en cas de vol</h4>
+      <p class="mb-8 text-gray-600">En cas de vol, nous remplaçons votre appareil.</p>
     </div>
     <div class="w-full md:w-1/2">
       <img src="{process.env.NODE_ENV === 'development' ? '' : '/velo.club'}/bicycle.svg" alt="Syncing" />
